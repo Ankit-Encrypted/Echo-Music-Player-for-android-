9a002369b582e445ecef567ca7cc2159eabcd969
Ankit Jain <akjain6067.aj@gmail.com>
2020-05-19 22:29:57 +0530

    Initial Build 1.0

---
+++ app/src/main/java/internshala/com/echomusik/activties/about_us_image.kt
@@ -0,0 +1,13 @@
+package internshala.com.echomusik.activties
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import internshala.com.echomusik.R
+
+
+class about_us_image : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about_us_image)
+    }
+}
